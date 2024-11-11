8ff5055992e271867ff735a85e56981c0d437075
Daniel <mihhail.kakourov@ivkhk.ee>
2024-11-11 12:49:20 +0200

    http

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const obj = require("./user")
+const http = require("http")
 
-console.log(obj.user)
+const server = http.createServer((req, res) => {
+    console.log(req.url)
 
-obj.sayHello()
+    res.write("<h1>Hello World</h1>")
+    res.write("<h2>Hello World</h2>")
+    res.write("<h3>Hello World</h3>")
+    res.end(`
+        <div style="background-color: red; width: 200px; height: 200px">
+        <h1>test</h1>
+        </div>
+    `)
+})
 
-console.log("work dir", __dirname)
-console.log("file name", __filename)
+server.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
