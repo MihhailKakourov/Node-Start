729d582355c115079477cfd66e06804763e290c0
Daniel <mihhail.kakourov@ivkhk.ee>
2024-11-11 12:41:33 +0200

    Events 2

--- refs/events_ref.js
+++ refs/events_ref.js
@@ -1,18 +1,36 @@
-const EventEmmitter = require("events");
+const EventEmitter = require("events");
 
-class Logger extends EventEmmitter {
-    log(msg) {
-        console.log(msg)
-        this.emit("message", `${msg} ${Date.now()}`)
+// class Logger extends EventEmitter {
+//     log(msg) {
+//         console.log(msg)
+//         this.emit("message", `${msg} ${Date.now()}`)
+//     }
+// }
+
+// const logger = new Logger()
+
+// logger.on("message", (msg) => {
+//     console.log(msg)
+// })
+
+// logger.log("Hello")
+// logger.log("Hello")
+// logger.log("Hello")
+
+
+class Notifier extends EventEmitter {
+    notify(message) {
+        console.log("Notification sent:", message);
+        this.emit("notification", `${message} at ${new Date().toLocaleTimeString()}`);
     }
 }
 
-const logger = new Logger()
+const notifier = new Notifier();
 
-logger.on("message", (msg) => {
-    console.log(msg)
-})
+notifier.on("notification", (msg) => {
+    console.log("Received notification:", msg);
+});
 
-logger.log("Hello")
-logger.log("Hello")
-logger.log("Hello")
+notifier.notify("You have a new message");
+notifier.notify("Your order has been shipped");
+notifier.notify("Meeting starts in 15 minutes");
