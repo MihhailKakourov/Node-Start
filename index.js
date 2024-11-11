4e76d0fd35fc8785a7d52d500a1a7a0d4fe3a369
Daniel <mihhail.kakourov@ivkhk.ee>
2024-11-11 14:07:56 +0200

    Simple Web-server with pages

--- index.js
+++ index.js
@@ -1,39 +1,65 @@
-const http = require("http")
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    if (req.method === "GET") {
-        res.writeHead(200,{
-            "Content-Type": "text/html"
-        })
+  if (req.method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8",
+    });
 
-        res.end(`
-            <h1>Form</h1>
-            <form method="POST" action="/">
-                <input type="text" name="name">
-                <button type="submit">Send</button>
-            </form>
-            `)
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "views", "index.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) {
+            throw err;
+          }
+          res.end(content);
+        }
+    );
+    } else if (req.url === "/about") {
+      fs.readFile(
+        path.join(__dirname, "views", "about.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) {
+            throw err;
+          }
+          res.end(content);
+        }
+      );
+    } else if (req.url==="/shop") {
+        fs.readFile(
+          path.join(__dirname, "views", "shop.html"),
+          "utf-8",
+          (err, content) => {
+            if (err) {
+              throw err;
+            }
+            res.end(content);
+          }
+        )
+      }
     } else if (req.method === "POST") {
-        const body = []
-        res.writeHead(200, {
-            "Content-Type": "text/html; charset=utf-8"
-        })
+      const body = [];
+      res.writeHead(200, {
+        "content-type": "text/html; charset=utf-8",
+      });
 
-        req.on("data", data => {
-            body.push(Buffer.from(data))
-        })
+      req.on("data", data => {
+        body.push(Buffer.from(data));
+      });
 
-        req.on("end", () => {
-            const message = body.toString().split("=")[1]
-
-            res.end(`
-                <h1>Form</h1>
-                <p>Name: ${message}</p>
-            `)
-        })
-    }
-})
+      req.on("end", () => {
+        const message = body.toString().split("=")[1];
+        res.end(`<h1>Ваше сообщение: ${message}</h1>
+        `);
+    });
+  }
+});
 
 server.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log("Server is running");
+});
